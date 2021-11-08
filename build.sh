6af012a242904b6f06c8c535e0acdd808b48d6c2
Anton Ovinnikov <anton.ovinnikov@yola.com>
2021-11-08 21:22:19 +0200

    better naming

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-env GOOS=darwin GOARCH=amd64 go build  -o ./bin/yoecwid-cli-macosx yoecwid-cli
-env GOOS=linux GOARCH=amd64 go build -o ./bin/yoecwid-cli-linux64 yoecwid-cli
-env GOOS=windows GOARCH=amd64 go build -o ./bin/yoecwid-cli-win yoecwid-cli
+env GOOS=darwin GOARCH=amd64 go build  -o ./bin/yoecwid-cli-mac-amd64 yoecwid-cli
+env GOOS=linux GOARCH=amd64 go build -o ./bin/yoecwid-cli-linux-amd64 yoecwid-cli
+env GOOS=windows GOARCH=amd64 go build -o ./bin/yoecwid-cli-windows-amd64 yoecwid-cli
